695e58639757d4b11af77d84a35b16efbb362efd
Rakan Hoji <73101833+RakanHoji@users.noreply.github.com>
2025-10-25 00:59:19 -0700

    Add Dataset definitions for tickers and predictions

---
+++ airflow/dags/dag_db.py
@@ -0,0 +1,7 @@
+from airflow import Dataset
+
+tickers_db_api = Dataset("/stock-Market_Predicitive-Analytics/") # Original Data 
+
+ticker_db_predction = Dataset("/stock-Market_Predicitive-Analytics/") # Database after predition
+
+
